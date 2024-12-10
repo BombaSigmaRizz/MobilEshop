bc15aefa40159edc3b24fd89266e2ef6204f5b62
Simon Adamcik <adamcik.ssimon@gmail.com>
2024-12-10 20:12:55 +0100

    farba

--- components/TopBar.vue
+++ components/TopBar.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="topbar">
-    <div class="right">Account</div>
-    <div class="basket"></div>
+    <div class="right">
+      <div class="account">Account</div>
+      <div class="basket"><Icon name="ph:basket" size="2.5rem"/></div>
+    </div>
   </div>
 </template>
 
@@ -12,9 +14,26 @@
 <style scoped lang="scss">
 .topbar {
   display: flex;
+  position: fixed;
+  top: 0;
+  left: 0;
   background-color: var(--bg-l);
   width: 100%;
-  height: 3rem;
+  height: 3.2rem;
+  box-sizing: border-box;
+  padding: 0 1rem;
   outline: 2px solid var(--border);
+  align-items: center;
+
+  div {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+  .right {
+    margin-left: auto;
+    display: flex;
+    gap: 1rem;
+  }
 }
 </style>
